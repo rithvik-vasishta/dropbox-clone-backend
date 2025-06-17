785f3f4a3ea07bca6e4c1041f4d88ba200005231
Rithvik Vasishta <rithvikvasishta@gmail.com>
2025-06-16 20:38:41 -0700

    safe migration

--- migrations/03_make_filename_unique.sql
+++ migrations/03_make_filename_unique.sql
@@ -1,5 +1,20 @@
 BEGIN;
-ALTER TABLE file_metadata
-    ADD CONSTRAINT file_metadata_filename_unique
-        UNIQUE (filename);
+DELETE FROM file_metadata a
+    USING file_metadata b
+WHERE a.id > b.id
+  AND a.filename = b.filename;
 COMMIT;
+DO $$
+    BEGIN
+        IF NOT EXISTS (
+            SELECT 1
+            FROM pg_constraint
+            WHERE conrelid = 'file_metadata'::regclass
+              AND conname  = 'file_metadata_filename_unique'
+        ) THEN
+            ALTER TABLE file_metadata
+                ADD CONSTRAINT file_metadata_filename_unique
+                    UNIQUE (filename);
+        END IF;
+    END
+$$;
